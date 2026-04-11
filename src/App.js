eed0d41f0460fd1a29bd9a3dd9d31fb9a1065994
tojibayevvv <abdullohspersonaldevelopment.28@gmail.com>
2026-04-11 13:26:50 +0500

    worked on 'split a bill' and showing who's paying what amount

--- src/App.js
+++ src/App.js
@@ -150,17 +150,32 @@ function FriendForm({ onAddFriend }) {
 }
 
 function FormSplitBill({ selected }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("");
   return (
     <form className="form-split-bill">
       <h2>Split the bill with {selected.name}</h2>
       <label>Bill Value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
       <label>Your expense</label>
-      <input type="text" disabled />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) => setPaidByUser(Number(e.target.value))}
+      />
       <label>{selected.name}'s expense</label>
-      <input type="text" />
+      <input type="text" disabled value={paidByFriend} />
       <label>Who's paying?</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">{selected.name}</option>
       </select>
